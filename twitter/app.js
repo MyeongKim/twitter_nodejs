c54cd03458c2948dc2a8e19808631afc0d243176
mingkim <ming3772@gmail.com>
2015-05-04 10:29:26 +0900

    button for share added

--- twitter/app.js
+++ twitter/app.js
@@ -61,20 +61,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use( function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', "null");
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-	// intercept OPTIONS method
-	if ('OPTIONS' == req.method) {
-	res.send(200);
-	}
-	else {
-	next();
-	}
-	});
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
